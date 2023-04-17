793ee8d83cf7846b82379f4ee6dafa6acafc0ff5
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-04-18 01:49:01 +0500

    Tiny refactoring

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi! I'm the most accurate weather forecaster on Telegram.")
+    await message.answer("Hi! I'm the most accurate weather forecaster on Telegram.")
 
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
-    await message.reply("Just send me the name of the city and I'll provide you with up-to-date weather info.\n\n"
-                        'Available cities:\n'
-                        '1. Saint-Petersburg, Russia\n'
-                        '2. Philadelphia, United States')
+    await message.answer("Just send me the name of the city and I'll provide you with up-to-date weather info.\n\n"
+                         'Available cities:\n'
+                         '1. Saint-Petersburg, Russia\n'
+                         '2. Philadelphia, United States')
 
 
 @dp.message_handler(regexp='(?i)^saint[ -]petersburg$')
